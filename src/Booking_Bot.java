a1971da758f70e844b1ef44037c7d6fc440f259a
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2022-04-27 02:06:07 +0800

    aa

---
+++ src/Booking_Bot.java
@@ -0,0 +1,56 @@
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class Booking_Bot {
+	WebDriver driver;
+	
+	public void setBrowser() {		
+		System.setProperty("webdriver.chrome.driver", "C:\\Jars\\chromedriver.exe");		
+		HashMap<String,Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("plugins.always_open_pdf_externally", true);
+		chromePrefs.put("download.default_directory", "C:"+File.separator+"Square_download");
+		chromePrefs.put("excludeSwitches", "enable-popup-blocking");	
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", chromePrefs);
+		
+		
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
+	}
+	Booking_Bot(){
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
