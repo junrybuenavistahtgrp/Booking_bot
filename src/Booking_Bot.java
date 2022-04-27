18a2c007f6d24e1e4a0953aa059703cb5f31324d
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2022-04-27 10:42:05 +0800

    a

--- src/Booking_Bot.java
+++ src/Booking_Bot.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
@@ -26,9 +27,21 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
 
 
-public class Booking_Bot {
-	WebDriver driver;
+public class Booking_Bot extends Thread{
 	
+	WebDriver driver;
+	SimpleDateFormat dateformat;
+	Statement st;
+	String Hotels[] = {
+						"B Ocean Resort, Fort Lauderdale, Florida, United States",
+						"Sea Club Ocean Resort, Fort Lauderdale, Florida, United States",
+						"Bahia Mar - Fort Lauderdale Beach - DoubleTree by Hilton, Fort Lauderdale, Florida, United States",
+						"Premiere Hotel, Fort Lauderdale, Florida, United States"};
+	
+	String links[] = {"//*[@id=\\\"search_results_table\\\"]/div/div/div/div/div[6]/div[2]/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/div/div[2]/span",
+			          "//*[@id=\"search_results_table\"]/div/div/div/div/div[6]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/div/div[2]/span",
+			          "//*[@id=\"search_results_table\"]/div/div/div/div/div[6]/div[2]/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/div/div[2]/span"};
+					
 	public void setBrowser() {		
 		System.setProperty("webdriver.chrome.driver", "C:\\Jars\\chromedriver.exe");		
 		HashMap<String,Object> chromePrefs = new HashMap<String, Object>();
@@ -40,16 +53,117 @@ public class Booking_Bot {
 		
 		
 		driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
 	Booking_Bot(){
+		dateformat = new SimpleDateFormat("yyyy-MM-dd");
+	}
+	public void run() {
+		String curDate ="";		
+		System.out.println(curDate);
 		
+		while(true) {
+			try {
+				Thread.sleep(2000);
+			}catch(Exception ee) {ee.printStackTrace();}
+			System.out.println("waiting");
+			
+			if(!curDate.equalsIgnoreCase(dateformat.format(new Date()))) {
+				setBrowser();
+			    String queryMe = "INSERT INTO Booking (Date, B_Ocean_Price, Sea_Club_Price, Bahia_Mar_Price, Premiere_Price) VALUES (value1, value2, value3,)";
+				curDate = dateformat.format(new Date());
+				for(int i=0;i<Hotels.length;i++) {
+					System.out.print("Proccessing "+Hotels[i]);
+					driver.get("https://www.booking.com/");
+					driver.findElement(By.id("ss")).clear();
+					driver.findElement(By.id("ss")).sendKeys(Hotels[i]);
+					
+					
+					try {
+						Thread.sleep(5000);
+					}catch(Exception ee) {ee.printStackTrace();}
+					
+					
+					if(i == 0) {
+						driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/div[1]/div[2]/div[1]/div[3]/div/div/div/div/span")).click();
+								
+								WebElement table = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div/div[3]/div[1]/table"));
+								List<WebElement> col = table.findElements(By.xpath(".//tbody/tr"));
+								
+								int col2;
+								for(int i1=0;i1<col.size()+1;i1++) {
+								
+								col2=i1;
+								List<WebElement> date = table.findElements(By.xpath(".//tbody/tr["+i1+"]/td"));
+									for(int ii=0;ii<date.size();ii++) {    		
+										
+										if(date.get(ii).getText().equalsIgnoreCase(new Date().getDate()+"")) {
+										driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div/div[3]/div[1]/table/tbody/tr["+col2+"]/td["+(ii+1)+"]")).click();																		
+										break;
+										}
+									}
+								}
+						}
+						
+						driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/div[1]/div[4]/div[2]/button/span[1]")).click();
+						String value = "";
+						int uselink = 0;
+						while(true) {
+							
+							try {
+								Thread.sleep(2000);
+							}catch(Exception ee) {ee.printStackTrace();}
+							
+							try {
+								value = driver.findElement(By.xpath(links[uselink])).getText();
+								                                     
+								break;
+							}catch(Exception ee) {uselink++;
+													if(uselink==links.length) {
+														value = "N/A";
+														break;
+													}
+													else {
+														System.out.print(" new link"+uselink);
+													} 
+												}
+						}
+						
+						
+						
+						System.out.println(" - "+ value);
+						
+						try {
+							Thread.sleep(4000);
+						}catch(Exception ee) {ee.printStackTrace();}
+					}
+					
+					
+					
+				}
+			}
 	}
 	
 	
+	public void setDataBaseConnection() {
+		while(true) {
+			System.out.println("Database connecting");
+			//gui.textAppend("Database connecting\n");
+			try{  
+				Thread.sleep(1500);
+				Class.forName("com.mysql.jdbc.Driver");  
+				Connection con=DriverManager.getConnection(  
+				"jdbc:mysql://localhost:3306/Booking_db","root","");  	
+				 st=con.createStatement();
+				 break;
+				
+			   }catch(Exception e){}
+		}
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Booking_Bot().start();
 
 	}
 
